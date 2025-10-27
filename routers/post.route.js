e94a9eebef4cd53e0128380a67ef347e059c23f3
pero4l <lilpero03@gmail.com>
2025-10-27 23:36:24 +0100

    blog post working

--- routers/post.route.js
+++ routers/post.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createPost, getAllPost, seeSinglePost, deletePost } = require('../controllers/postController');
+const { createPost, getAllPost, seeSinglePost, deletePost, comment } = require('../controllers/postController');
 const{authMiddleware} = require('../middlewares/authUserMiddlewar')
 
 router.post('/create', authMiddleware, createPost);
 router.get('/all', authMiddleware, getAllPost);
 router.delete('/delete', authMiddleware, deletePost)
+router.post('/comment/:id', authMiddleware, comment);
 router.get('/:id', authMiddleware, seeSinglePost);
 
 
